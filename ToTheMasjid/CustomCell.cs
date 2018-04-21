16a95e48a4a9112e438960fb3843ad2d18974c07
Saamer Mansoor <saamer.mansoor@slalom.com>
2018-04-20 23:07:35 -0600

    CustomCell updates-GridLayout

--- ToTheMasjid/CustomCell.cs
+++ ToTheMasjid/CustomCell.cs
@@ -5,6 +5,74 @@ namespace ToTheMasjid
 {
 	public class CustomCell : ViewCell
 	{
+		public void CustomCell1()
+		{
+			//Practicing Grids
+			Grid grid = new Grid
+			{
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				RowDefinitions =
+				{
+				    new RowDefinition { Height = GridLength.Auto },
+				    new RowDefinition { Height = GridLength.Auto },
+				    new RowDefinition { Height = new GridLength(1, GridUnitType.Star) },
+				    //new RowDefinition { Height = new GridLength(4, GridUnitType.Absolute) }
+				},
+				ColumnDefinitions =
+				{
+				    new ColumnDefinition { Width = GridLength.Auto },
+				    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+				    //new ColumnDefinition { Width = new GridLength(4, GridUnitType.Absolute) }
+				}
+			};
+
+			//grid.Children.Add(new Label
+			//{
+			//	Text = "Grid",
+			//	FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+			//	HorizontalOptions = LayoutOptions.Center
+			//}, 0, 3, 0, 1);
+			//left, right, top, bottom
+			//or just left, top
+
+			grid.Children.Add(new Label
+			{
+				Text = " ",
+				TextColor = Color.White,
+				BackgroundColor = Color.Blue
+			}, 0, 1);
+
+			grid.Children.Add(new BoxView
+			{
+				Color = Color.Silver,
+				//HeightRequest = 0
+			}, 1, 1);
+
+			grid.Children.Add(new BoxView
+			{
+				Color = Color.Teal
+			}, 0, 2);
+
+			grid.Children.Add(new Label
+			{
+				Text = "Leftover space",
+				TextColor = Color.Purple,
+				BackgroundColor = Color.Aqua,
+				HorizontalTextAlignment = TextAlignment.Center,
+				VerticalTextAlignment = TextAlignment.Center,
+			}, 1, 2);
+			//grid.Children.Add(new Label
+			//{
+			//	Text = "Fixed 100x100",
+			//	TextColor = Color.Aqua,
+			//	BackgroundColor = Color.Red,
+			//	HorizontalTextAlignment = TextAlignment.Center,
+			//	VerticalTextAlignment = TextAlignment.Center
+			//}, 2, 3);
+			View = grid;
+		}
+
+		//Old CustomCell
 		public CustomCell()
 		{
 			// Create views with bindings for displaying each property.

36ecc8a19a2d11f620222b6277310c5d3d3d9bac
Saamer Mansoor <saamer.mansoor@slalom.com>
2018-04-20 22:47:02 -0600

    Added a next icon, Improved the ListView

--- ToTheMasjid/CustomCell.cs
+++ ToTheMasjid/CustomCell.cs
@@ -10,41 +10,78 @@ namespace ToTheMasjid
 			// Create views with bindings for displaying each property.
 			var mosqueNameLabel = new Label();
 			mosqueNameLabel.SetBinding(Label.TextProperty, "name");
+			var x = mosqueNameLabel.Width;
+			var mosqueDistanceLabel = new Label(){Text="\t <1 mile"};
 
-			var nextPrayerLabel = new Label();
+			var nextPrayerName = new Label();
 			//birthdayLabel.SetBinding(Label.TextProperty,
 			//new Binding("AsrTime", BindingMode.OneWay,
 			//null, null, "Born {0:d}"));
-			nextPrayerLabel.Text = "Next: Maghreb. 15 minutes more";
+			nextPrayerName.Text = "Next: Maghreb.";
 			var image = new Image()
 			{
 				Source = "Pray32.png"
 			};
 			//boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
 
+			var nextPrayerTime = new Label() { Text = "\t 2:15pm" };
 
-			var stack = new StackLayout()
+			var midUpperStack = new StackLayout()
 			{
-				Padding = new Thickness(0, 5),
 				Orientation = StackOrientation.Horizontal,
 				Children =
-			    {
-					image,
-				new StackLayout
 				{
-				    VerticalOptions = LayoutOptions.Center,
-				    Spacing = 0,
-				    Children =
-				    {
 					mosqueNameLabel,
-					nextPrayerLabel
-				    }
+					mosqueDistanceLabel
 				}
-			    }
+			};
+			var midLowerStack = new StackLayout()
+			{
+				Orientation = StackOrientation.Horizontal,
+				Children =
+				{
+					nextPrayerName,
+					nextPrayerTime
+				}
+			};
+			var midWholeStack = new StackLayout()
+			{
+				VerticalOptions = LayoutOptions.Center,
+				Spacing = 0,
+				Children =
+				{
+					midUpperStack,
+					midLowerStack
+				}
+			};
+			var nextLabel = new Label()
+			{
+				Text = "\t >", 
+				TextColor = Color.Silver
+			};
+			var nextPage = new StackLayout()
+			{
+				VerticalOptions = LayoutOptions.Center,
+				Spacing = 2,
+				Children =
+				{
+					nextLabel
+				}
+			};
+			var mainStack = new StackLayout()
+			{
+				Padding = new Thickness(0.5, 5),
+				Orientation = StackOrientation.Horizontal,
+				Children =
+				{
+					image,
+					midWholeStack,
+					nextPage
+				}
 			};
 
 			//instantiate each of our views
-			View = stack;
+			View = mainStack;
 		}
 
 	}

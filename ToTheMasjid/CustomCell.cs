f1646a30fb3789169c8798d40fdca8e1981f4fd9
Saamer Mansoor <saamer.mansoor@slalom.com>
2018-04-20 22:28:57 -0600

    MosquesPage listView stack improvement

--- ToTheMasjid/CustomCell.cs
+++ ToTheMasjid/CustomCell.cs
@@ -7,11 +7,44 @@ namespace ToTheMasjid
 	{
 		public CustomCell()
 		{
+			// Create views with bindings for displaying each property.
+			var mosqueNameLabel = new Label();
+			mosqueNameLabel.SetBinding(Label.TextProperty, "name");
+
+			var nextPrayerLabel = new Label();
+			//birthdayLabel.SetBinding(Label.TextProperty,
+			//new Binding("AsrTime", BindingMode.OneWay,
+			//null, null, "Born {0:d}"));
+			nextPrayerLabel.Text = "Next: Maghreb. 15 minutes more";
+			var image = new Image()
+			{
+				Source = "Pray32.png"
+			};
+			//boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
+
+
+			var stack = new StackLayout()
+			{
+				Padding = new Thickness(0, 5),
+				Orientation = StackOrientation.Horizontal,
+				Children =
+			    {
+					image,
+				new StackLayout
+				{
+				    VerticalOptions = LayoutOptions.Center,
+				    Spacing = 0,
+				    Children =
+				    {
+					mosqueNameLabel,
+					nextPrayerLabel
+				    }
+				}
+			    }
+			};
+
 			//instantiate each of our views
-			Label lblJoke = new Label();
-			lblJoke.LineBreakMode = LineBreakMode.WordWrap;
-			lblJoke.SetBinding(Label.TextProperty, "name");
-			View = lblJoke;
+			View = stack;
 		}
 
 	}
--- ToTheMasjid/UI/AboutPage.cs
+++ ToTheMasjid/UI/AboutPage.cs
@@ -6,23 +6,38 @@ namespace ToTheMasjid
 	{
 		public AboutPage()
 		{
-			Title = "About Page";
-
-			// Use this to Add a get current location Target or GPS arrow button 
+			Title = "About";
 			//var toolbarItem = new ToolbarItem
 			//{
 			//	Icon = "Logout.png"
 			//};
 			//toolbarItem.Clicked += OnLogoutButtonClicked;
 			//ToolbarItems.Add(toolbarItem);
+			BackgroundColor = Color.FromRgb(136, 189, 86);
 
-			BackgroundColor = Color.FromRgb(136, 72, 190);
+			var mainLayout = new StackLayout();
 
 			var layout1 = new StackLayout()
 			{
-				BackgroundColor = Color.White,
+				BackgroundColor = Color.FromHex("99CE78"),
 				Opacity = 0.75,
-				Padding = new Thickness(10, 40, 10, 40),
+				Padding = new Thickness(0, 40, 0, 40),
+				VerticalOptions = LayoutOptions.FillAndExpand
+			};
+			var logo = new Image()
+			{
+				Source = "Pray64.png",
+				HeightRequest = 64,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				HorizontalOptions = LayoutOptions.FillAndExpand
+			};
+			layout1.Children.Add(logo);
+
+			var layout2 = new StackLayout()
+			{
+				BackgroundColor = Color.White,
+				Spacing = 10,
+				Padding = new Thickness(16, 40, 16, 40),
 				VerticalOptions = LayoutOptions.FillAndExpand
 			};
 
@@ -31,19 +46,58 @@ namespace ToTheMasjid
 				FontSize = 22
 			};
 			var s1 = new FormattedString();
-			s1.Spans.Add(new Span { Text = "ToTheMasjid", FontAttributes = FontAttributes.Bold, FontSize = 24 });
+			s1.Spans.Add(new Span { Text = "To The Masjid", FontAttributes = FontAttributes.Bold, FontSize = 24 });
 			s1.Spans.Add(new Span { Text = " " });
 			s1.Spans.Add(new Span { Text = "1.0", ForegroundColor = Color.Gray });
 
 			label1.FormattedText = s1;
-			layout1.Children.Add(label1);
-			var label2 = new Label()
+
+			var label2 = new Label();
+			var s2 = new FormattedString();
+			s2.Spans.Add(new Span
 			{
-				Text = "About Page"
+				Text = "This app serves the as a tool to find the nearest mosque and the next prayer time there for ",
+			});
+			s2.Spans.Add(new Span
+			{
+				Text = "Muslims",
+				FontAttributes = FontAttributes.Bold
+			});
+			s2.Spans.Add(new Span { Text = " in Denver" });
+			label2.FormattedText = s2;
+
+			var label3 = new Label();
+			var s3 = new FormattedString();
+			s3.Spans.Add(new Span
+			{
+				Text = "Stay tuned, because more feaures like ",
+			});
+			s3.Spans.Add(new Span
+			{
+				Text = "Push Notifications",
+				FontAttributes = FontAttributes.Bold
+			});
+			s3.Spans.Add(new Span
+			{
+				Text = " is on its way!"
+			});
+			label3.FormattedText = s3;
+
+			var debugTapGestureRecognizer = new TapGestureRecognizer();
+			debugTapGestureRecognizer.Tapped += (s, e) =>
+			{
+
+				DisplayAlert("Debug Mode", "Good job", "OK");
 			};
-			layout1.Children.Add(label2);
-			var mainLayout = new StackLayout();
+			//tapGestureRecognizer3.NumberOfTapsRequired = 5;
+			label3.GestureRecognizers.Add(debugTapGestureRecognizer);
+
+			layout2.Children.Add(label1);
+			layout2.Children.Add(label2);
+			layout2.Children.Add(label3);
+
 			mainLayout.Children.Add(layout1);
+			mainLayout.Children.Add(layout2);
 			Content = new ScrollView
 			{
 				Content = mainLayout
